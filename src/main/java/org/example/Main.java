667e367480c5b4ee1490d2657cfe4346ae1f7163
christopher.bayquen <christopher.bayquen@gcash.com>
2025-07-03 15:36:46 +0800

    Implemented main menu

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,14 +4,46 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.print("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("==== BANK MENU ====");
+        System.out.println("1. Create Account \n2.View All Accounts \n3. Check Balance \n4. Deposit \n5. Withdraw \n6. Exit");
+
+        while (true){
+            System.out.println("\nEnter Chosen Option: ");
+            int userChoice;
+            try{
+                userChoice = Integer.parseInt(scanner.nextLine());
+            } catch (NumberFormatException e) {
+                System.out.print("> [ERR: Please Enter a Number]");
+                continue;
+            }
+
+            switch(userChoice){
+                case 1:
+                    System.out.println("==== Create Account ====");
+                    break;
+                case 2:
+                    System.out.println("==== View All Accounts ====");
+                    break;
+                case 3:
+                    System.out.println("==== Check Balance ====");
+                    break;
+                case 4:
+                    System.out.println("==== Deposit ====");
+                    break;
+                case 5:
+                    System.out.println("==== Withdraw ====");
+                    break;
+                case 6:
+                    System.out.println("> [EXITING: Thank you for using Bank!]");
+                    return;
+                default:
+                    System.out.println("> [ERR: Invalid Choice Input]");
+
+
+            }
+
         }
     }
 }
